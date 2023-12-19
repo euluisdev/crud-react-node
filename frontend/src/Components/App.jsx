53e5348cd2e384c64bc3d6faf829652130fac977
Luis Fernando <fluisf00@gmail.com>
2023-12-19 11:30:15 -0300

    added inputs-button-function handleClicBtn

--- frontend/src/Components/App.jsx
+++ frontend/src/Components/App.jsx
@@ -1,11 +1,36 @@
 import { useState } from 'react'
 import '/src/css/App.css'
 
-function App() {
+const App = () => {
+  const handleClickBtn = () => {
+    console.log('Clicou!')
+  };
 
-  return (
-    <div>Hi wordão!</div>
-  )
-}
+  return <div className='app--container'>
+    <div className="register--container">
+      <h1 className="register--title">Screen Shop</h1>
+      <input type="text" 
+        name='name' 
+        placeholder='Home' 
+        className='register--input' 
+      />
+      <input type="text" 
+        name='name' 
+        placeholder='Home' 
+        className='register--input' 
+      />
+      <input type="text" 
+        name='name' 
+        placeholder='Home' 
+        className='register--input' 
+      />
+      <button 
+        className='register--btn' 
+        onClick={() => handleClickBtn()}>Cadastrar
+      </button>
+    </div>
+  </div>
+  
+};
 
 export default App;
