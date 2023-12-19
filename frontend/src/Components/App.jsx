0ae76c4fbab7d9a37a66fa8d22d411f45bef4881
Luis Fernando <fluisf00@gmail.com>
2023-12-19 13:30:17 -0300

    create state and function handleChangeValues

--- frontend/src/Components/App.jsx
+++ frontend/src/Components/App.jsx
@@ -1,36 +1,51 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
 import '/src/css/App.css'
 
 const App = () => {
+  const [values, setValues] = useState();
+  console.log(values);
+
+  const handleChangeValues = (event) => {
+    setValues(prevEvent => ({
+      ...prevEvent,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
   const handleClickBtn = () => {
     console.log('Clicou!')
   };
 
-  return <div className='app--container'>
-    <div className="register--container">
-      <h1 className="register--title">Screen Shop</h1>
-      <input type="text" 
-        name='name' 
-        placeholder='Home' 
-        className='register--input' 
-      />
-      <input type="text" 
-        name='name' 
-        placeholder='Home' 
-        className='register--input' 
-      />
-      <input type="text" 
-        name='name' 
-        placeholder='Home' 
-        className='register--input' 
-      />
-      <button 
-        className='register--btn' 
-        onClick={() => handleClickBtn()}>Cadastrar
-      </button>
+  return (
+    <div className='app--container'>
+      <div className="register--container">
+        <h1 className="register--title">Screen Shop</h1>
+        <input type="text"
+          name='name'
+          placeholder='Home'
+          className='register--input' 
+          onChange={handleChangeValues}
+        />
+        <input type="text"
+          name='name'
+          placeholder='Home'
+          className='register--input' 
+          onChange={handleChangeValues}
+        />
+        <input type="text"
+          name='name'
+          placeholder='Home'
+          className='register--input' 
+          onChange={handleChangeValues}
+        />
+        <button
+          className='register--btn'
+          onClick={() => handleClickBtn()}>Cadastrar
+        </button>
+      </div>
     </div>
-  </div>
-  
+  )
+
 };
 
 export default App;
