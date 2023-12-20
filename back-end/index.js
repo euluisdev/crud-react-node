e8df4c02403aa791fd1dc77cade071b96be5677f
Luis Fernando <fluisf00@gmail.com>
2023-12-20 13:08:16 -0300

    create example SQL statement to inlustrate data insertion

--- back-end/index.js
+++ back-end/index.js
@@ -6,6 +6,20 @@ const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT || 3002;
 
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    database: "crudregister"
+});
+
+app.get("/", (req, res) => {
+    let SQL = "INSERT INTO games ( name, cost, category ) VALUES ( 'Far Cry', '120', 'Ação' )";
+
+    db.query(SQL, (err, result) => {
+        console.log(err);
+    });
+});
+
 app.listen(port, () => {
     console.log('server connected!');
 });
