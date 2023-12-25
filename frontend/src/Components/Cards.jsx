d02c5ca81e1ce0ca7fc3dd05f53af89407626c05
Luis Fernando <fluisf00@gmail.com>
2023-12-25 20:17:22 -0300

    create file Card.css end added cod

--- frontend/src/Components/Cards.jsx
+++ frontend/src/Components/Cards.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import '/src/css/Cards.css';
 
 const Card = (props) => {
     return ( 
         <div className="card--container">
             <h1 className="card--title">{props.name}</h1>
-            <p className="card--cost">{props.cost}</p>
             <p className="card--category">{props.category}</p>
+            <p className="card--cost">R$ {props.cost}</p>
         </div>
     )
 };
