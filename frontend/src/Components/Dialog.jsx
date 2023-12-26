7ac4b04e9a35d577a7665746e6ee82499be93e00
Luis Fernando <fluisf00@gmail.com>
2023-12-26 09:55:33 -0300

    create handleEditeGame function end state

--- frontend/src/Components/Dialog.jsx
+++ frontend/src/Components/Dialog.jsx
@@ -5,6 +5,7 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
+import Axios from 'axios';
 
 export default function FormDialog(props) {
   const [editValues, setEditValues] = useState({
@@ -13,6 +14,15 @@ export default function FormDialog(props) {
     cost: props.cost,
     category: props.category,
   });
+
+  const handleEditGame = () => {
+    Axios.put("http://localhost3002/edit", {
+      id: editValues.id,
+      name: editValues.name,
+      cost: editValues.cost,
+      category: editValues.category,
+    })
+  };
 
   const handleClickOpen = () => {
     props.setOpen(true);
@@ -24,57 +34,57 @@ export default function FormDialog(props) {
 
   const handleChangeValues = (value) => {
     setEditValues(prevValues => ({
-      ...prevValues, 
+      ...prevValues,
       [value.target.id]: value.target.value,
     }));
   };
 
   return (
-      <Dialog 
-        open={props.open} 
-        onClose={handleClose}
-      >
-        <DialogTitle>Editar</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus 
-            margin="dense" 
-            id="name" 
-            label="Nome do jogo" 
-            defaultValue={props.name} 
-            onChange={handleChangeValues} 
-            type="text"
-            fullWidth
-            variant="standard"
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="category"
-            label="Categoria"
-            defaultValue={props.category} 
-            onChange={handleChangeValues} 
-            type="text"
-            fullWidth
-            variant="standard"
-          />
-          <TextField
-            autoFocus 
-            margin="dense" 
-            id="cost" 
-            label="Preço" 
-            defaultValue={props.cost} 
-            onChange={handleChangeValues} 
-            type="text" 
-            fullWidth
-            variant="standard"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Salvar</Button>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Excluir</Button>
-        </DialogActions>
-      </Dialog>
+    <Dialog
+      open={props.open}
+      onClose={handleClose}
+    >
+      <DialogTitle>Editar</DialogTitle>
+      <DialogContent>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          label="Nome do jogo"
+          defaultValue={props.name}
+          onChange={handleChangeValues}
+          type="text"
+          fullWidth
+          variant="standard"
+        />
+        <TextField
+          autoFocus
+          margin="dense"
+          id="category"
+          label="Categoria"
+          defaultValue={props.category}
+          onChange={handleChangeValues}
+          type="text"
+          fullWidth
+          variant="standard"
+        />
+        <TextField
+          autoFocus
+          margin="dense"
+          id="cost"
+          label="Preço"
+          defaultValue={props.cost}
+          onChange={handleChangeValues}
+          type="text"
+          fullWidth
+          variant="standard"
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleEditGame}>Salvar</Button>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleClose}>Excluir</Button>
+      </DialogActions>
+    </Dialog>
   );
 }
